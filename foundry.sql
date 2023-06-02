c0755c5413010eea6a2b8913b6778439fee585ef
Vidyadheesh <vidyadheeshp@gmail.com>
2023-06-02 14:34:51 +0530

    updated sql file

--- foundry.sql
+++ foundry.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 14, 2023 at 09:01 AM
+-- Generation Time: Jun 02, 2023 at 11:04 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.28
 
@@ -20,6 +20,34 @@ SET time_zone = "+00:00";
 --
 -- Database: `foundry`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `core_shop_details`
+--
+
+CREATE TABLE `core_shop_details` (
+  `sno` int(11) NOT NULL,
+  `core_type` int(11) NOT NULL,
+  `product_name` varchar(64) NOT NULL,
+  `core_weight` int(11) NOT NULL,
+  `no_of_core_produced` int(11) NOT NULL,
+  `no_of_defect_cores` int(11) NOT NULL,
+  `Date` date NOT NULL,
+  `shift` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `core_shop_details`
+--
+
+INSERT INTO `core_shop_details` (`sno`, `core_type`, `product_name`, `core_weight`, `no_of_core_produced`, `no_of_defect_cores`, `Date`, `shift`, `created_at`, `updated_at`, `status`) VALUES
+(1, 1, 'Key component', 452, 500, 20, '2023-06-08', 2, '2023-06-02 11:11:30', '0000-00-00 00:00:00', 1),
+(2, 1, 'bridge tame', 452, 500, 20, '2023-06-14', 1, '2023-06-02 11:12:36', '0000-00-00 00:00:00', 1);
 
 -- --------------------------------------------------------
 
@@ -77,7 +105,9 @@ INSERT INTO `melting_details_master` (`sno`, `heat_no`, `type_of_melting`, `rr`,
 (5, '32456', 1, 45, 34, 87, 56, 49, 65423, '2023-01-14 12:24:48', '0000-00-00 00:00:00', 1),
 (6, '32456', 1, 45, 34, 87, 56, 49, 65423, '2023-01-14 12:24:48', '0000-00-00 00:00:00', 1),
 (7, '3q45', 1, 765, 345, 34, 546, 43, 543, '2023-01-14 12:26:02', '0000-00-00 00:00:00', 1),
-(8, '3q45', 1, 765, 345, 34, 546, 43, 543, '2023-01-14 12:26:02', '0000-00-00 00:00:00', 1);
+(8, '3q45', 1, 765, 345, 34, 546, 43, 543, '2023-01-14 12:26:02', '0000-00-00 00:00:00', 1),
+(9, 'sdfth546', 2, 45, 54, 56, 23, 43, 4545, '2023-01-16 04:33:25', '0000-00-00 00:00:00', 1),
+(10, 'drtyj', 2, 45, 456, 6, 65, 5, 67, '2023-01-16 04:34:17', '0000-00-00 00:00:00', 1);
 
 -- --------------------------------------------------------
 
@@ -141,7 +171,7 @@ CREATE TABLE `quality_inspection_master` (
   `sno` int(11) NOT NULL,
   `product_id` varchar(128) NOT NULL,
   `casting_weight` int(11) NOT NULL,
-  `defects` varchar(32) NOT NULL,
+  `defects` int(11) NOT NULL,
   `location` varchar(128) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
@@ -153,13 +183,53 @@ CREATE TABLE `quality_inspection_master` (
 --
 
 INSERT INTO `quality_inspection_master` (`sno`, `product_id`, `casting_weight`, `defects`, `location`, `created_at`, `updated_at`, `status`) VALUES
-(1, '12jhgvf122', 50, '1-2', 'public_html/Laballocator/ajax', '2023-01-13 04:36:57', '0000-00-00 00:00:00', 1),
-(2, 'bnh20230115', 50, '1-2', 'road way', '2023-01-13 04:49:10', '0000-00-00 00:00:00', 1),
-(3, 'bnh20230115', 50, '1-2', 'road way', '2023-01-13 04:49:46', '0000-00-00 00:00:00', 1),
-(4, '12jhgvf122', 50, '2-3', 'public_html/Laballocator/ajax', '2023-01-13 04:55:58', '0000-00-00 00:00:00', 1),
-(5, '12jhgvf122', 100, '2-3', 'casting', '2023-01-13 04:56:53', '0000-00-00 00:00:00', 1),
-(6, '210389bnvrd', 65, '2', 'casting', '2023-01-13 04:58:00', '0000-00-00 00:00:00', 1),
-(7, 'sgbvf456', 100, '1-2', 'melting', '2023-01-13 04:58:54', '0000-00-00 00:00:00', 1);
+(1, '12jhgvf122', 50, 1, 'public_html/Laballocator/ajax', '2023-01-13 04:36:57', '0000-00-00 00:00:00', 1),
+(2, 'bnh20230115', 50, 1, 'road way', '2023-01-13 04:49:10', '0000-00-00 00:00:00', 1),
+(3, 'bnh20230115', 50, 1, 'road way', '2023-01-13 04:49:46', '0000-00-00 00:00:00', 1),
+(4, '12jhgvf122', 50, 2, 'public_html/Laballocator/ajax', '2023-01-13 04:55:58', '0000-00-00 00:00:00', 1),
+(5, '12jhgvf122', 100, 2, 'casting', '2023-01-13 04:56:53', '0000-00-00 00:00:00', 1),
+(6, '210389bnvrd', 65, 2, 'casting', '2023-01-13 04:58:00', '0000-00-00 00:00:00', 1),
+(7, 'sgbvf456', 100, 1, 'melting', '2023-01-13 04:58:54', '0000-00-00 00:00:00', 1),
+(8, '2108202110A', 25, 2, 'near the core', '2023-06-01 10:35:58', '0000-00-00 00:00:00', 1),
+(9, 'ery45', 50, 1, 'near the core', '2023-06-01 02:33:04', '0000-00-00 00:00:00', 1),
+(10, '12jhgvf122', 50, 2, 'dxfb', '2023-06-01 02:33:31', '0000-00-00 00:00:00', 1),
+(11, '12jhgvf122', 50, 1, 'df', '2023-06-01 02:34:08', '0000-00-00 00:00:00', 1),
+(12, '123testing', 50, 1, 'casting', '2023-06-01 02:37:51', '0000-00-00 00:00:00', 1),
+(13, '123testing', 50, 3, 'casting', '2023-06-01 02:37:51', '0000-00-00 00:00:00', 1),
+(14, '20123429hqig4', 100, 11, 'casting', '2023-06-01 03:19:20', '0000-00-00 00:00:00', 1),
+(15, '20123429hqig4', 100, 12, 'casting', '2023-06-01 03:19:20', '0000-00-00 00:00:00', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sand_testing_details_master`
+--
+
+CREATE TABLE `sand_testing_details_master` (
+  `sno` int(11) NOT NULL,
+  `per_moisture` int(11) NOT NULL,
+  `per_clay` int(11) NOT NULL,
+  `other_additives` int(11) NOT NULL,
+  `shear_strength` int(11) NOT NULL,
+  `permiability` int(11) NOT NULL,
+  `active_clay` int(11) NOT NULL,
+  `dead_clay` int(11) NOT NULL,
+  `GFN` int(11) NOT NULL,
+  `GCS` int(11) NOT NULL,
+  `compatibility` int(11) NOT NULL,
+  `mould_hardness` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sand_testing_details_master`
+--
+
+INSERT INTO `sand_testing_details_master` (`sno`, `per_moisture`, `per_clay`, `other_additives`, `shear_strength`, `permiability`, `active_clay`, `dead_clay`, `GFN`, `GCS`, `compatibility`, `mould_hardness`, `created_at`, `updated_at`, `status`) VALUES
+(1, 54, 5, 4, 85, 684, 5284, 68, 65, 654, 64, 654, '2023-01-17 12:20:06', '0000-00-00 00:00:00', 1),
+(2, 57, 6543, 365, 6, 544, 65, 564, 654, 564, 564, 8597, '2023-01-17 12:21:16', '0000-00-00 00:00:00', 1);
 
 -- --------------------------------------------------------
 
@@ -213,6 +283,12 @@ INSERT INTO `user_type` (`sno`, `u_type`, `status`) VALUES
 --
 
 --
+-- Indexes for table `core_shop_details`
+--
+ALTER TABLE `core_shop_details`
+  ADD PRIMARY KEY (`sno`);
+
+--
 -- Indexes for table `department_master`
 --
 ALTER TABLE `department_master`
@@ -243,6 +319,12 @@ ALTER TABLE `quality_inspection_master`
   ADD PRIMARY KEY (`sno`);
 
 --
+-- Indexes for table `sand_testing_details_master`
+--
+ALTER TABLE `sand_testing_details_master`
+  ADD PRIMARY KEY (`sno`);
+
+--
 -- Indexes for table `user_master`
 --
 ALTER TABLE `user_master`
@@ -259,6 +341,12 @@ ALTER TABLE `user_type`
 --
 
 --
+-- AUTO_INCREMENT for table `core_shop_details`
+--
+ALTER TABLE `core_shop_details`
+  MODIFY `sno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `department_master`
 --
 ALTER TABLE `department_master`
@@ -268,7 +356,7 @@ ALTER TABLE `department_master`
 -- AUTO_INCREMENT for table `melting_details_master`
 --
 ALTER TABLE `melting_details_master`
-  MODIFY `sno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `sno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `meta_core_type`
@@ -286,7 +374,13 @@ ALTER TABLE `meta_defect_type`
 -- AUTO_INCREMENT for table `quality_inspection_master`
 --
 ALTER TABLE `quality_inspection_master`
-  MODIFY `sno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `sno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `sand_testing_details_master`
+--
+ALTER TABLE `sand_testing_details_master`
+  MODIFY `sno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user_master`
